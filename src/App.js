33e0cc83789dfab8beab1edd182a50b7be6e5bb2
Eosdia <lumimomen@gmail.com>
2023-04-17 00:12:24 +0900

    create css btn, title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 function App() {
   return (
     <div>
-      <h1>Welcom Back!!!</h1>
+      <h1 className={styles.title}>Welcom Back!!!</h1>
       <Button text={"Continue"} />
     </div>
   );
