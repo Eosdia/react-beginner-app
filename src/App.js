c18b2510373261c0700e550699b80baed6c86afe
Eosdia <lumimomen@gmail.com>
2023-04-18 16:24:08 +0900

    create Todo component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Button from "./Button";
+import Todo from "./Todo";
 import styles from "./App.module.css";
 function App() {
   return (
     <div>
       <h1 className={styles.title}>Welcom Back!!!</h1>
       <Button text={"Continue"} />
+      <Todo />
     </div>
   );
 }
